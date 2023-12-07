2df9c4813f640794f3b853ac9a3abaea026f940c
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-07 14:33:28 -0300

    Update PyGamesHannaJullie.py

--- PyGamesHannaJullie.py
+++ PyGamesHannaJullie.py
@@ -1,1 +1,8 @@
-lal
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+
+st.set_page_config(page_title="PyGames",page_icon="🕹️")
+st.sidebar.header("PyGames de Hanna")
+st.header('PyGames', divider='pink')
